d5e078b82ecd637624cef0c3da7b1a9053d97ed8
Luke Madel <luke.madel@gmail.com>
2013-08-20 09:45:27 +0200

    first import

---
+++ index.js
@@ -0,0 +1,160 @@
+'use strict';
+
+var path = require('path')
+, basename = path.basename(path.dirname(__filename))
+, debug = require('debug')('mill:contrib:' + basename)
+, async = require('async')
+, Segmenter = require('segmenter')
+, Transform = require("stream").Transform
+;
+
+function Command(options)
+{
+  Transform.call(this, options);
+  this.begin = true;
+  this.seg = new Segmenter({delimiter: options.delimiter});
+  this.lines =  options.lines || 10;
+  this.offset =  options.offset || 0;
+  this.invert = options.invert || false;
+  this.counter = 0;
+  this.disabled = false;
+  this.remainder = 0;
+}
+
+Command.prototype = Object.create(
+  Transform.prototype, { constructor: { value: Command }});
+
+Command.prototype.parse = function (rows, done) {
+  var r;
+
+
+  if (Array.isArray(rows)) {
+    var start = this.counter;
+    var begin = this.offset - start;
+    var large = begin + this.lines;
+    var depth = (this.lines - this.remainder) + start;
+    this.counter += rows.length;
+    debug('start', start)
+    debug('begin', begin)
+    debug('large', large)
+    debug('depth', depth)
+    debug('lines', this.lines)
+    debug('offset', this.offset)
+    debug('counter', this.counter)
+    debug('length', rows.length)
+    debug('remainder', this.remainder)
+    if (!this.invert) {
+      if (this.offset > this.counter) {
+        debug('case #1');
+        //            [===========>
+        // |--------|
+        r = '';
+      }
+      else if (this.offset < this.counter && large >= this.counter && this.remainder === 0) {
+        debug('case #2');
+        //   [===========>
+        // |--------|
+        r = rows.slice(begin).join(this.seg.delimiter());
+        this.remainder = this.counter - this.offset;
+      }
+      else if (this.offset < this.counter && large < this.counter && this.remainder === 0) {
+        debug('case #3');
+        //   [====>
+        // |--------|
+        r = rows.slice(begin, begin + this.lines).join(this.seg.delimiter());
+        this.remainder = 0;
+      }
+      else if (this.offset <= start && depth <= this.counter && this.remainder > 0) {
+        debug('case #4');
+        //   [===========>
+        // |........|--------|
+        r = rows.slice(0, this.lines - this.remainder).join(this.seg.delimiter());
+        this.remainder = 0;
+      }
+      else if (this.offset <= start && depth > this.counter && this.remainder > 0) {
+        debug('case #5');
+        //   [==================>
+        // |........|--------|
+        r = rows.join(this.seg.delimiter());
+        this.remainder += rows.length;
+      }
+      else if (this.offset < start && this.offset + this.lines < start && this.remainder === 0) {
+        debug('case #6');
+        //   [===========>
+        //                   |--------|
+        r = '';
+        this.remainder = 0;
+      }
+    }
+    else {
+      if (this.offset > this.counter) {
+        debug('case i#1');
+        //            [===========>
+        // |--------|
+        r = rows.join(this.seg.delimiter());
+        this.remainder = 0;
+      }
+      else if (this.offset < this.counter && large >= this.counter && this.remainder === 0) {
+        debug('case i#2');
+        //   [===========>
+        // |--------|
+        r = rows.slice(0, begin).join(this.seg.delimiter());
+        this.remainder = this.counter - this.offset;
+      }
+      else if (this.offset < this.counter && large < this.counter && this.remainder === 0) {
+        debug('case i#3');
+        //   [====>
+        // |--------|
+        r = rows.slice(0, begin).concat(rows.slice(begin + this.lines)).join(this.seg.delimiter());
+        this.remainder = 0;
+      }
+      else if (this.offset <= start && depth <= this.counter && this.remainder > 0) {
+        debug('case i#4');
+        //   [===========>
+        // |........|--------|
+        r = rows.slice(this.lines - this.remainder).join(this.seg.delimiter());
+        this.remainder = 0;
+      }
+      else if (this.offset <= start && depth > this.counter && this.remainder > 0) {
+        debug('case i#5');
+        //   [==================>
+        // |........|--------|
+        r = '';
+        this.remainder += rows.length;
+      }
+      else if (this.offset < start && this.offset + this.lines < start && this.remainder === 0) {
+        debug('case i#6');
+        //   [===========>
+        //                   |--------|
+        r = rows.join(this.seg.delimiter());
+        this.remainder = 0;
+      }
+    }
+    if (r) {
+      r += this.seg.delimiter();
+      this.push(r);
+    }
+  }
+  done();
+};
+
+
+Command.prototype._transform = function (chunk, encoding, done) {
+  if (this.begin) {
+    this.begin = false;
+    this.emit('begin');
+  }
+  this.parse(this.seg.fetch(chunk, encoding), done);
+}
+Command.prototype.end = function () {
+  var that = this;
+  that.parse(that.seg.fetch(), function () {
+      that.emit('end');
+    }
+  );
+};
+
+module.exports = function (options, si) {
+  var cmd = new Command(options);
+  return si.pipe(cmd);
+}
---
+++ test.js
@@ -0,0 +1,346 @@
+'use strict';
+var  path = require('path')
+, basename = path.basename(path.dirname(__filename))
+, util = require('util')
+, should = require('should')
+, tester = require('mill-core').tester
+, command = require('./index.js')
+;
+
+
+describe(basename, function () {
+
+    var prepend = ''
+
+    describe('PUT empty string', function () {
+        it('should return 400', function (done) {
+            tester(command, {})
+            .send(' \n')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal(' \n')
+                done()
+              }
+            )
+          }
+        )
+      }
+    )
+
+    describe('PUT string without expression', function () {
+        it('should return 200', function (done) {
+            tester(command, {})
+            .send('xxx\n')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal('xxx\n')
+                done()
+              }
+            )
+          }
+        )
+      }
+    )
+
+    describe('PUT simple string', function () {
+        it('should return one line', function (done) {
+            tester(command, {lines : 1})
+            .send('aaa\nbbb\nccc\n')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal('aaa\n')
+                done()
+              }
+            )
+          }
+        )
+        it('should return two line', function (done) {
+            tester(command, {lines : 2})
+            .send('aaa\nbbb\nccc\n')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal('aaa\nbbb\n')
+                done()
+              }
+            )
+          }
+        )
+        it('should return three lines', function (done) {
+            tester(command, {lines : 3})
+            .send('aaa\nbbb\nccc\n')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal('aaa\nbbb\nccc\n')
+                done()
+              }
+            )
+          }
+        )
+        it('should return three lines (limit is over)', function (done) {
+            tester(command, {lines : 4})
+            .send('aaa\nbbb\nccc\n')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal('aaa\nbbb\nccc\n')
+                done()
+              }
+            )
+          }
+        )
+        it('should return two line (invert)', function (done) {
+            tester(command, {lines : 1, invert : true})
+            .send('aaa\nbbb\nccc\n')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal('bbb\nccc\n')
+                done()
+              }
+            )
+          }
+        )
+        it('should return one line (invert)', function (done) {
+            tester(command, {lines : 2, invert : true})
+            .send('aaa\nbbb\nccc\n')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal('ccc\n')
+                done()
+              }
+            )
+          }
+        )
+
+        it('should return zero line (invert)', function (done) {
+            tester(command, {lines : 3, invert : true})
+            .send('aaa\nbbb\nccc\n')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal('')
+                done()
+              }
+            )
+          }
+        )
+
+        it('should return zero line (invert) (limit is over)', function (done) {
+            tester(command, {lines : 4, invert : true})
+            .send('aaa\nbbb\nccc\n')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal('')
+                done()
+              }
+            )
+          }
+        )
+
+
+
+      }
+    )
+    describe('PUT simple string (change delimiter)', function () {
+        it('should return one line', function (done) {
+            tester(command, {lines : 1, delimiter : "\\t"})
+            .send('aaa\tbbb\tccc')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal('aaa\t')
+                done()
+              }
+            )
+          }
+        )
+        it('should return two line', function (done) {
+            tester(command, {lines : 2, delimiter : "\\t"})
+            .send('aaa\tbbb\tccc')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal('aaa\tbbb\t')
+                done()
+              }
+            )
+          }
+        )
+        it('should return two line (invert)', function (done) {
+            tester(command, {lines : 1, invert : true, delimiter : "\\t"})
+            .send('aaa\tbbb\tccc')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal('bbb\tccc\t')
+                done()
+              }
+            )
+          }
+        )
+        it('should return one line (invert)', function (done) {
+            tester(command, {lines : 2, invert : true, delimiter : "\\t"})
+            .send('aaa\tbbb\tccc')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal('ccc\t')
+                done()
+              }
+            )
+          }
+        )
+
+      }
+    )
+    describe('PUT simple string (offset changing)', function () {
+        it('should return one line', function (done) {
+            tester(command, {lines : 1, offset : 1})
+            .send('aaa\nbbb\nccc\n')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal('bbb\n')
+                done()
+              }
+            )
+          }
+        )
+      }
+    )
+    describe('PUT medium string (offset changing)', function () {
+        
+        it('should return one line', function (done) {
+            for (var i = 1, prepend = ''; i <= 20000; i++) {
+              prepend += 'x\n';
+            }
+            tester(command, {lines : 1, offset : 20001})
+            .send(prepend + 'aaa\nbbb\nccc\n')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal('bbb\n')
+                done()
+              }
+            )
+          }
+        )
+      }
+    )
+
+    describe('PUT large string (offset changing)', function () {
+        it('should return one line', function (done) {
+            for (var i = 1, prepend = ''; i <= 100000; i++) {
+              prepend += 'x\n'
+            }
+            tester(command, {lines : 1, offset : 100001})
+            .send(prepend + 'aaa\nbbb\nccc\n')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal('bbb\n')
+                done()
+              }
+            )
+          }
+        )
+        it('should return two line', function (done) {
+            for (var i = 1, prepend = ''; i <= 100000; i++) {
+              prepend += 'x\n'
+            }
+            tester(command, {lines : 2, offset : 100001})
+            .send(prepend + 'aaa\nbbb\nccc\n')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal('bbb\nccc\n')
+                done()
+              }
+            )
+          }
+        )
+        it('should return three line', function (done) {
+            for (var i = 1, prepend = ''; i <= 100000; i++) {
+              prepend += 'x\n'
+            }
+            tester(command, {lines : 3, offset : 100000})
+            .send(prepend + 'aaa\nbbb\nccc\n')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal('aaa\nbbb\nccc\n')
+                done()
+              }
+            )
+          }
+        )
+      }
+    )
+
+    describe('PUT simple string (offset changing & invert)', function () {
+        it('should return two line', function (done) {
+            tester(command, {lines : 1, offset : 1, invert: true})
+            .send('aaa\nbbb\nccc\n')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal('aaa\nccc\n')
+                done()
+              }
+            )
+          }
+        )
+      }
+    )
+    describe('PUT medium string (offset changing & invert)', function () {
+
+        it('should ignore one line', function (done) {
+            for (var i = 1, prepend = ''; i <= 20000; i++) {
+              prepend += 'x\n';
+            }
+            tester(command, {lines : 1, offset : 20001, invert: true})
+            .send(prepend + 'aaa\nbbb\nccc\n')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal(prepend + 'aaa\nccc\n')
+                done()
+              }
+            )
+          }
+        )
+      }
+    )
+    describe('PUT large string (offset changing & invert)', function () {
+        it('should ignore one line', function (done) {
+            for (var i = 1, prepend = ''; i <= 100000; i++) {
+              prepend += 'x\n'
+            }
+            tester(command, {lines : 1, offset : 100001, invert: true})
+            .send(prepend + 'aaa\nbbb\nccc\n')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal(prepend + 'aaa\nccc\n')
+                done()
+              }
+            )
+          }
+        )
+        it('should return two line', function (done) {
+            for (var i = 1, prepend = ''; i <= 100000; i++) {
+              prepend += 'x\n'
+            }
+            tester(command, {lines : 2, offset : 100001, invert: true})
+            .send(prepend + 'aaa\nbbb\nccc\n')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal(prepend + 'aaa\n')
+                done()
+              }
+            )
+          }
+        )
+        it('should ignore three line', function (done) {
+            for (var i = 1, prepend = ''; i <= 100000; i++) {
+              prepend += 'x\n'
+            }
+            tester(command, {lines : 3, offset : 100000, invert: true})
+            .send(prepend + 'aaa\nbbb\nccc\n')
+            .end(function (err, res) {
+                should.not.exist(err)
+                res.should.equal(prepend)
+                done()
+              }
+            )
+          }
+        )
+      }
+    )
+  }
+);
